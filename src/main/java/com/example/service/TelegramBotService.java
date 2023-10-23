7e48d68d8320f4d7b07f99f89eabbed82887ed45
Sam Hosseini <sam.hosseini256@gmail.com>
2023-10-23 12:11:55 +0330

    First Sample spring boot Echo telegram robot - id: mohajer13bot

--- src/main/java/com/example/service/TelegramBotService.java
+++ src/main/java/com/example/service/TelegramBotService.java
@@ -1,31 +1,38 @@
 package com.example.service;
 
-import com.example.Configs.BotInitialize;
 import lombok.extern.log4j.Log4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Log4j
 @Component
 public class TelegramBotService extends TelegramLongPollingBot {
 
-    @Autowired
-    private BotInitialize botInitialize;
+    @Value("${bot.username}")
+    private String botUsername;
 
-    @Override
-    public String getBotUsername() {
-        return botInitialize.getBotUsername();
-    }
+    @Value("${bot.token}")
+    private String botToken;
 
     @Override
     public String getBotToken() {
-        return botInitialize.getBotToken();
+        return botToken;
     }
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -39,13 +46,24 @@ public class TelegramBotService extends TelegramLongPollingBot {
 
     }
 
-    public void sendAnswerMessage(SendMessage message) {
+    private void sendAnswerMessage(SendMessage message) {
         if (message != null) {
             try {
                 execute(message);
             } catch (TelegramApiException e) {
                 log.error(e);
             }
+        }
+    }
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(this);
+        }
+        catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
         }
     }
 
